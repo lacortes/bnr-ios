f5432e51369e08e6065f05ea537e2dc682fedd94
Luis Cortes <cortes72luis@gmail.com>
2020-07-01 21:02:39 -0700

    Added some more stuff

--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -7,11 +7,83 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
+    
+    var mapView: MKMapView!
+        
+    override func loadView() {
+        // Create a map view
+        mapView = MKMapView()
+        
+        // Set it as "the" view of this view controller
+        view = mapView
+        
+        // Add segmented controls
+        let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+        segmentedControl.backgroundColor = UIColor.systemBackground
+        segmentedControl.selectedSegmentIndex = 0
+        
+        segmentedControl.addTarget(self, action: #selector(mapTypeChanged(_:)), for: .valueChanged)
+        
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(segmentedControl)
+        
+        let poiLabel = UILabel.init()
+        poiLabel.text = "Points of Interest"
+        
+        view.addSubview(poiLabel)
+        
+        let uiSwitch = UISwitch.init()
+        view.addSubview(uiSwitch)
+        
+        // Constrain segmented controls based on super view
+        segmentedControl.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8).isActive = true
+        
+        let margins = view.layoutMarginsGuide
+        segmentedControl.leadingAnchor.constraint(equalTo: margins.leadingAnchor).isActive = true
+        segmentedControl.trailingAnchor.constraint(equalTo: margins.trailingAnchor).isActive = true
+        
+        // Point of intereset
+        poiLabel.translatesAutoresizingMaskIntoConstraints = false
+        poiLabel.leadingAnchor.constraint(equalTo: margins.leadingAnchor).isActive = true
+        poiLabel.topAnchor.constraint(equalTo: segmentedControl.bottomAnchor, constant: 10).isActive = true
+        
+        // UI Switch
+        uiSwitch.translatesAutoresizingMaskIntoConstraints = false
+        uiSwitch.leadingAnchor.constraint(equalTo: poiLabel.trailingAnchor).isActive = true
+        uiSwitch.topAnchor.constraint(equalTo: segmentedControl.bottomAnchor, constant: 10).isActive = true
+        
+        uiSwitch.addTarget(self, action: #selector(mapSwitchChanged(_:)), for: .valueChanged)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         print("MapViewController loaded its view")
     }
+    
+    @objc func mapSwitchChanged(_ uiSwitch: UISwitch) {
+        if uiSwitch.isOn {
+            print("switch on!")
+        } else {
+            print("switch off")
+        }
+    }
+    
+    @objc func mapTypeChanged(_ segControl: UISegmentedControl) {
+        switch segControl.selectedSegmentIndex {
+        case 0:
+            mapView.mapType = .standard
+        case 1:
+            mapView.mapType = .hybrid
+        case 2:
+            mapView.mapType = .satellite
+        default:
+            break
+        }
+    }
+    
 }
